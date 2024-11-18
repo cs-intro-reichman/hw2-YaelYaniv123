65c45c9201a87ce9b5a474786b4d59ef312a1c80
Yael Yaniv <yaelyaniv@Yaels-MacBook-Air.local>
2024-11-18 22:07:32 +0200

    2Ready4

--- CalcPi.java
+++ CalcPi.java
@@ -3,17 +3,19 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    int numTerms = Integer.parseInt(args[0]);
 		double calc = 0.0;
-		int counter = 1;
+		int denominator = 1;
 		
 		for (int i = 0; i <= numTerms - 1; i++) {
-			calc = calc + (double) 1 / counter;
-			counter = counter + 2;
-			if (i + 1 <= numTerms) {
-				calc = calc - (double) 1 / counter;
-				counter = counter + 2;
-				i++;
+			if (i % 2 == 0){
+				calc = calc + (double) 1 / denominator;
+				denominator = denominator + 2;
+			}
+			else {
+				calc = calc - (double) 1 / denominator;
+				denominator = denominator + 2;
 			}
 		}
+
 		calc = calc * 4.0;
 		System.out.println("pi according to Java: " + Math.PI);
 		System.out.println("pi, approximated:     " + (double)calc);
